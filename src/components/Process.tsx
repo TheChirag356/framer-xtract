7a2d31fdb1d91520fb5e9eb3f6cfdb6d0e3c6a59
TheChirag356 <arorachirag12005@gmail.com>
2025-06-19 00:11:54 +0530

    feat: enhance Process component with new animations and visual elements

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -2,9 +2,24 @@
 import React from "react";
 import Badge from "@/components/Badge";
 import BentoBox from "@/components/BentoBox";
-import { Shield, Cpu, Gauge, Repeat, BicepsFlexed } from "lucide-react";
+import {
+  Shield,
+  Cpu,
+  Gauge,
+  Repeat,
+  BicepsFlexed,
+  Slack,
+  Bot,
+  Mail,
+} from "lucide-react";
 
 const Process = () => {
+  const stack = [
+    <Slack key={"slack"} className="aspect-square h-12 w-12 text-white" />,
+    <Mail key={"mail"} className="aspect-square h-12 w-12 text-white" />,
+    <Bot key={"bot"} className="aspect-square h-12 w-12 text-white" />,
+  ];
+
   return (
     <div className="flex min-h-[100dvh] w-[100dvw] flex-col items-center justify-start">
       <div className="flex h-[20rem] w-1/3 flex-col items-center justify-start gap-4 text-center">
@@ -88,6 +103,42 @@ const Process = () => {
             We smoothly integrate AI solutions into your existing infrastructure
             with minimal disruption.
           </p>
+          <div className="mt-2 flex h-full w-full items-center justify-between">
+            <div className="mx-auto my-auto flex h-full w-full items-center justify-around border border-neutral-800">
+              <div className="flex flex-col items-center justify-center gap-2">
+                <div className="relative aspect-square w-[4.5rem] rounded-md border border-neutral-800">
+                  <div
+                    className="absolute inset-0 h-12 w-12 translate-x-1/4 translate-y-1/4 animate-[spin_5s_linear_infinite] rounded-full blur-[2px]"
+                    style={{
+                      backgroundImage:
+                        "linear-gradient(229deg,var(--token-cd9ad879-0340-4881-8da6-7efa110062d4, #df7afe) 13%,rgba(201,110,240,0) 35.0235827429153%,rgba(164,92,219,0) 64.17244225559735%,var(--token-a888adc2-3d05-4d6f-88b7-8dab97418795, rgb(129, 74, 200)) 88%)",
+                    }}
+                  ></div>
+                  <div
+                    className="absolute inset-0 aspect-square h-8 translate-x-2/3 translate-y-2/3 animate-[spin_5s_linear_infinite_reverse] rounded-full blur-[2px]"
+                    style={{
+                      backgroundImage:
+                        "linear-gradient(229deg,var(--token-cd9ad879-0340-4881-8da6-7efa110062d4, #df7afe) 13%,rgba(201,110,240,0) 35.0235827429153%,rgba(164,92,219,0) 64.17244225559735%,var(--token-a888adc2-3d05-4d6f-88b7-8dab97418795, rgb(129, 74, 200)) 88%)",
+                    }}
+                  ></div>
+                </div>
+                <p className="text-[10px]">Our solution</p>
+              </div>
+              <div className="flex flex-col items-center justify-center gap-2">
+                <div className="items-items relative flex aspect-square max-h-[4.5rem] w-[4.5rem] flex-col justify-start gap-2 overflow-hidden rounded-md border border-neutral-800">
+                  {[...stack, stack[0]].map((item, idx) => (
+                    <div
+                      key={idx}
+                      className="mt-3 flex items-center justify-center"
+                    >
+                      {item}
+                    </div>
+                  ))}
+                </div>
+                <p className="text-[10px]">Your stack</p>
+              </div>
+            </div>
+          </div>
         </BentoBox>
         <BentoBox>
           <Badge text="Step 4" />
